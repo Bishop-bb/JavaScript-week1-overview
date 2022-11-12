4ccad8bcc8ae5b61b28ecf7dbd64b03c563a5ed4
Bishop b <iyiolablessing98@gmail.com>
2022-11-12 03:34:08 +0100

    finished operators

---
+++ ARITHEMETIC-OPERATORS/operator.js
@@ -0,0 +1,55 @@
+
+        document.getElementById('d7').innerHTML="OPERATORS";
+        document.getElementById('odd').innerHTML="the numbers in arithemetic operations are called operands while the operation to be peformed is defined by an operator";
+
+        document.getElementById('d0').innerHTML="Arithemetic Operators";
+        document.getElementById('d8').innerHTML="1. Addition";
+        
+
+        document.getElementById('d9').innerHTML="2. Subtraction";
+
+        document.getElementById('d10').innerHTML="3. Multiplication";
+
+        document.getElementById('d11').innerHTML="4. Division";
+
+        document.getElementById('d12').innerHTML="5. Modulus";
+
+        document.getElementById('d13').innerHTML="6. Exponential";
+
+        document.getElementById('inc').innerHTML="7. Incrementing";
+
+        document.getElementById('dec').innerHTML="8. Decrementing";
+
+        document.getElementById('op').innerHTML="9. Operator Precedence";
+        
+
+        let a = 4;
+        let b = 3;
+        let c = a + b;
+        let d = a - b;
+        let e = a * b;
+        let f = a / b;
+        let g = a % b;
+        let h = a ** b;
+        let i = 12;
+        i ++;
+        let j = i;
+        let k = 3;
+        k --;
+        let l = k;
+        let m = 20 + 5 * 4;
+        let n = m;
+
+
+
+
+
+        document.getElementById("add").innerHTML= c ;
+        document.getElementById("sub").innerHTML= d ;
+        document.getElementById("mul").innerHTML= e ;
+        document.getElementById("div").innerHTML= f ;
+        document.getElementById("mod").innerHTML= g ;
+        document.getElementById("exp").innerHTML= h ;
+        document.getElementById("ic").innerHTML= j ;
+        document.getElementById("dc").innerHTML= k ;
+        document.getElementById("opr").innerHTML= n ;

34b9c6aa1671edb5be3d230f818ccfe6f9129b51
Bishop b <iyiolablessing98@gmail.com>
2022-11-12 05:18:19 +0100

    i'm done

---
+++ COMPARISON/comparison.js
@@ -0,0 +1,54 @@
+document.getElementById('co').innerHTML="Comparison Operators";
+document.getElementById('coo').innerHTML="Comparison Operators are used in logical statements to determine equality or difference between variables and values";
+document.getElementById('given').innerHTML="Given that x = 10 , the table below explains the comparison operators";
+
+document.getElementById('c1').innerHTML="1. Operator ( == )";
+document.getElementById('c2').innerHTML="2. Operator ( === )";
+document.getElementById('c3').innerHTML="3. Operator ( != )";
+document.getElementById('c4').innerHTML="4. Operator ( !== )";
+document.getElementById('c5').innerHTML="5. Operator ( > )";
+document.getElementById('c6').innerHTML="6. Operator ( < )";
+document.getElementById('c7').innerHTML="7. Operator ( >= )";
+document.getElementById('c8').innerHTML="8. Operator ( >= )";
+
+
+
+
+
+let x = 10;
+
+let a = x == 10;
+let b = x == 7;
+let z = x == "10";
+
+let c = x === 10;
+let d = x === "10";
+
+let e = x != 10;
+
+let f = x !== 10;
+let g = x !== "10";
+let h = x !== 7;
+
+let i = x > 15;
+
+let j = x < 15;
+
+let k = x >= 15;
+
+let l = x <= 15;
+
+
+document.getElementById('c-1').innerHTML= a;
+document.getElementById('c-11').innerHTML= b;
+document.getElementById('c-111').innerHTML= z;
+document.getElementById('c-2').innerHTML= c;
+document.getElementById('c-22').innerHTML= d;
+document.getElementById('c-3').innerHTML= e;
+document.getElementById('c-4').innerHTML= f;
+document.getElementById('c-44').innerHTML= g;
+document.getElementById('c-444').innerHTML= h;
+document.getElementById('c-5').innerHTML= i;
+document.getElementById('c-6').innerHTML= j;
+document.getElementById('c-7').innerHTML= k;
+document.getElementById('c-8').innerHTML= l;

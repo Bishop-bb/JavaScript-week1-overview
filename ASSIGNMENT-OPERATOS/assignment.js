71130239cf1d80fb609eac92a07ac4806bb656a4
Bishop b <iyiolablessing98@gmail.com>
2022-11-12 04:06:00 +0100

    done with ass operators

---
+++ ASSIGNMENT-OPERATOS/assignment.js
@@ -0,0 +1,33 @@
+document.getElementById('ao').innerHTML="JavaScript Assignment Operators";
+document.getElementById('ass').innerHTML="Assignment operators are used to assign values to Javascript variables";
+document.getElementById('o1').innerHTML="1. Operator ( = )";
+document.getElementById('o2').innerHTML="2. Operator ( += )";
+document.getElementById('o3').innerHTML="3. Operator ( -= )";
+document.getElementById('o4').innerHTML="4. Operator ( *= )";
+document.getElementById('o5').innerHTML="5. Operator ( /= )"
+document.getElementById('o6').innerHTML="6. Operator ( %= )"
+document.getElementById('o7').innerHTML="7. Operator ( **= )"
+
+
+
+let a = 12;
+let b = 12;
+let c = a;
+
+let d = 12;
+let e = 3;
+let f = d += e;
+let g = d -= e;
+let h = d *= e;
+let i = d /= e;
+let j = d %= e;
+let k = d ** e;
+
+
+document.getElementById('b').innerHTML= c ;
+document.getElementById('o-2').innerHTML= f ;
+document.getElementById('o-3').innerHTML= g ;
+document.getElementById('o-4').innerHTML= h ;
+document.getElementById('o-5').innerHTML= i ;
+document.getElementById('o-6').innerHTML= j ;
+document.getElementById('o-7').innerHTML= k  ;
